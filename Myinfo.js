5b24d50d693a91db79e835d1733d4e3a4674c16b
Tharsan-Sandrakumar <tharsansandrakumar@gmail.com>
2019-02-07 12:58:14 +0000

    Update Myinfo.js

--- Myinfo.js
+++ Myinfo.js
@@ -2,3 +2,12 @@ function f() {
     document.getElementById("welcme").innerHTML = "Welcome To My Page";
     document.getElementById("welinfo").innerHTML = "TESTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT";
 }
+
+function inputNumber() {
+  var inpObj = document.getElementById("num1");
+  if (!inpObj.checkValidity()) {
+    document.getElementById("demo").innerHTML = inpObj.validationMessage;
+  } else {
+    document.getElementById("demo").innerHTML = "Input OK";
+  } 
+} 
